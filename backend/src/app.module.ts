59997681094a7b8d1f0bd2b2e33fc11674bcb984
italo.biondo <italo.biondo@defensoria.mg.def.br>
2025-12-01 16:50:05 -0300

    adicionando porta para teste de conexao banco - sprint 6

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -4,7 +4,6 @@ import { HealthModule } from './modules/health/health.module';
 import { UsersModule } from './modules/users/users.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { TodosModule } from './modules/todos/todos.module';
-import { TimerModule } from './modules/timer/timer.module';
 
 @Module({
   imports: [
@@ -13,7 +12,6 @@ import { TimerModule } from './modules/timer/timer.module';
     UsersModule,
     AuthModule,
     TodosModule,
-    TimerModule,
   ],
 })
 export class AppModule {}
